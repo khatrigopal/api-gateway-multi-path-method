9fdf673cc146b3d9d0e6a71b0487d6c91841c836
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-06 05:16:46 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "example_api_gateway" {
+  source = "./modules/api-gateway"
+
+  name        = "example-api"
+  description = "Example API Gateway REST API"
+
+  resources = {
+    "resource1" = {
+      parent_id = aws_api_gateway_rest_api.example_api.root_resource_id
+      path_part = "resource1"
+    }
+    "resource2" = {
+      #parent_id = aws_api_gateway_resource.example_resource["resource1"].id
+      parent_id = aws_api_gateway_resource.example_api.root_resource.id 
+      path_part = "resource2"
+    }
+  }
+
+  methods = {
+    "method1" = {
+      resource_name = "resource1"
+      http_method   = "POST"
+      authorization = "NONE"
+    }
+    "method2" = {
+      resource_name = "resource2"
+      http_method   = "POST"
+      authorization = "NONE"
+    }
+  }
