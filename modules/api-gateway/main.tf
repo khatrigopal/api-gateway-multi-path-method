826523c4846b7694d397f6d4362cb1363d3c047f
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-06 05:12:58 +0100

    Create main.tf

---
+++ modules/api-gateway/main.tf
@@ -0,0 +1,21 @@
+resource "aws_api_gateway_rest_api" "example_api" {
+  name        = var.name
+  description = var.description
+}
+
+resource "aws_api_gateway_resource" "example_resource" {
+  for_each = var.resources
+
+  rest_api_id = aws_api_gateway_rest_api.example_api.id
+  parent_id   = each.value.parent_id
+  path_part   = each.value.path_part
+}
+
+resource "aws_api_gateway_method" "example_method" {
+  for_each = var.methods
+
+  rest_api_id   = aws_api_gateway_rest_api.example_api.id
+  resource_id   = aws_api_gateway_resource.example_resource[each.value.resource_name].id
+  http_method   = each.value.http_method
+  authorization = each.value.authorization
+}
