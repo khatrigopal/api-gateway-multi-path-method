3e29069f5265892af216b8fe7f62c78e6de3474a
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-06 06:34:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ module "example_api_gateway" {
   description = "Example API Gateway REST API"
 
   resources = {
-    "resource1" = {
+    "/resource1" = {
       #parent_id = aws_api_gateway_rest_api.example_api.root_resource1_id
       #resource_name = "resource1"
       parent_id = "module.example_api_gateway.root_resource_id"
       path_part = "resource1"
     }
-    "resource2" = {
+    "/resource2" = {
       #parent_id = aws_api_gateway_resource.example_resource["resource1"].id
       #parent_id = aws_api_gateway_resource.example_api.root_resource2.id 
       #resource_name = "resource2"
